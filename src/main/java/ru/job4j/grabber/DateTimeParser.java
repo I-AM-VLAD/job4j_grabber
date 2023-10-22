2ae9d887e1237a42e0db32e45e42d95981620d0e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2023-10-22 11:34:38 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/DateTimeParser.java
+++ src/main/java/ru/job4j/grabber/DateTimeParser.java
@@ -3,7 +3,5 @@ package ru.job4j.grabber;
 import java.time.LocalDateTime;
 
 public interface DateTimeParser {
-    static LocalDateTime parse(String parse) {
-        return null;
-    }
+    LocalDateTime parse(String parse);
 }
--- src/main/java/ru/job4j/grabber/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/HabrCareerDateTimeParser.java
@@ -7,27 +7,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-    public static String splt(String in) {
-        if (in.contains("+")) {
-            String[] words = in.split("\\+");
-            return words[0];
-        } else {
-            return null;
-        }
-    }
-
     @Override
-    public static LocalDateTime parse(String parse) {
-        LocalDateTime dateTime = LocalDateTime.parse(splt(parse));
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-ddТhh:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(parse, formatter);
         System.out.println(dateTime);
         return dateTime;
     }
-
     public static void main(String[] args) {
-        System.out.println(splt("2018-05-05T11:50:55+03:00"));
+        System.out.println(parse("2018-05-05T11:50:55+03:00"));
     }
 }
 
  */
+
 
 
